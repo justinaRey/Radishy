565f2e46e1ba96096a782d9e89f95d05d109dfe8
facuh5 <facu5herraez@gmail.com>
2021-11-03 19:32:48 -0300

    primer controlador 'login'

--- Radishy/src/main/java/egg/Radishy/Controladores/SesionIniciada_controller.java
+++ Radishy/src/main/java/egg/Radishy/Controladores/SesionIniciada_controller.java
@@ -6,6 +6,7 @@
 package egg.Radishy.Controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,5 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("")
 public class SesionIniciada_controller {
   
-    
+    @GetMapping("")
+    public String login(){
+        return "usuario.hmtl";
+    }
 }
