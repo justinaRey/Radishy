a48c0d25d693fd548c0942fb4f8e5460e215ef18
facuh5 <facu5herraez@gmail.com>
2021-11-09 16:10:27 -0300

    entidad cultivo adaptada a 'mi versión'

--- Radishy/src/main/java/egg/Radishy/entidades/Cultivo.java
+++ Radishy/src/main/java/egg/Radishy/entidades/Cultivo.java
@@ -3,14 +3,11 @@ package egg.Radishy.entidades;
 
 import egg.Radishy.enumeraciones.Mes;
 import java.io.Serializable;
-import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -28,14 +25,23 @@ public class Cultivo implements Serializable {
     
     @Enumerated(EnumType.STRING)
     private Mes finEpocaSiembra;
-    private String metodo;
-    private Integer profSiembraCM;
-    private Integer tiempoGerminar;
-    private Integer tiempoTransplantar;
-    private Integer tiempoCosechar;
     
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date fechaDeSiembra;
+    private String metodo;
+    private Integer profundidadSiembraCM;
+    
+    private Integer tiempoGerminarMin; // en reemplazo a tiempoGerminar, se ponen dos con el tiempo min y max que puede tardar
+    private Integer tiempoGerminarMax;
+    //private String tiempoGerminar; // concatenación de t.germinar min + max junto con la palabra días
+    
+    private Integer tiempoTransplantarMin;
+    private Integer tiempoTransplantarMax;
+    //private String tiempoParaTransplantar; // concatenación de ambos tiempos de transplante + la palabra días
+    
+    private Integer tiempoCosecharMin;
+    private Integer tiempoCosecharMax;
+    //private String tiempoCosechar; // concatenación de tiempo de cosecha min y max sumado a la palabra días
+    
+    // Saqué el fechaDeSembrado porque lo incorporé a la sesión iniciada
 
     public String getId() {
         return id;
@@ -77,45 +83,149 @@ public class Cultivo implements Serializable {
         this.metodo = metodo;
     }
 
-    public Integer getProfSiembraCM() {
-        return profSiembraCM;
+    public Integer getProfundidadSiembraCM() {
+        return profundidadSiembraCM;
     }
 
-    public void setProfSiembraCM(Integer profSiembraCM) {
-        this.profSiembraCM = profSiembraCM;
+    public void setProfundidadSiembraCM(Integer profSiembraCM) {
+        this.profundidadSiembraCM = profSiembraCM;
     }
 
-    public Integer getTiempoGerminar() {
-        return tiempoGerminar;
+    public Integer getTiempoGerminarMin() {
+        if (tiempoGerminarMin != null){
+            return tiempoGerminarMin;
+        } else {
+            return 0;
+        }
     }
 
-    public void setTiempoGerminar(Integer tiempoGerminar) {
-        this.tiempoGerminar = tiempoGerminar;
+    public void setTiempoGerminarMin(Integer tiempoGerminar) {
+        this.tiempoGerminarMin = tiempoGerminar;
     }
 
-    public Integer getTiempoTransplantar() {
-        return tiempoTransplantar;
+    public Integer getTiempoGerminarMax() {
+        if (tiempoGerminarMax != null){
+            return tiempoGerminarMax;
+        } else {
+           return 0; 
+        }
     }
 
-    public void setTiempoTransplantar(Integer tiempoTransplantar) {
-        this.tiempoTransplantar = tiempoTransplantar;
-    }
-
-    public Integer getTiempoCosechar() {
-        return tiempoCosechar;
-    }
-
-    public void setTiempoCosechar(Integer tiempoCosechar) {
-        this.tiempoCosechar = tiempoCosechar;
-    }
-
-    public Date getFechaDeSiembra() {
-        return fechaDeSiembra;
-    }
-
-    public void setFechaDeSiembra(Date fechaDeSiembra) {
-        this.fechaDeSiembra = fechaDeSiembra;
+    public void setTiempoGerminarMax(Integer tiempoGerminar) {
+        this.tiempoGerminarMax = tiempoGerminar;
     }
     
+//    public void setTiempoGerminar(){
+//        this.tiempoGerminar = "";
+//        if (getTiempoGerminarMin != 0){
+//           this.tiempoGerminar = String.ValueOff(getTiempoGerminarMin);
+//        }
+//        if (getTiempoGerminarMax != 0) {
+//            if(getTiempoGerminarMin == 0){    
+//               this.tiempoGerminar = String.ValueOff(getTiempoGerminarMax);
+//            } else {
+//               this.tiempoGerminar = this.tiempoGerminar.concat(" - ");
+//               this.tiempoGerminar = this.tiempoGerminar.concat(getTiempoGerminarMax);
+//            }
+//        }
+//        if (getTiempoGerminarMin == 0 && getTiempoGerminarMax == 0){
+//            this.tiempoGerminar.concat(" - ");
+//        } else {
+//            this.tiempoGerminar.concat(" días");
+//    }
     
+//    public String getTiempoGerminar () {
+//          return this.tiempoGerminar;
+    public Integer getTiempoTransplantarMin() {
+        if (this.tiempoTransplantarMin != null){
+            return tiempoTransplantarMin;
+        } else {
+            return 0;
+        }
+    }
+
+    public void setTiempoTransplantarMin(Integer tiempoTransplantar) {
+        this.tiempoTransplantarMin = tiempoTransplantar;
+    }
+
+    public Integer getTiempoTransplantarMax() {
+        if (this.tiempoTransplantarMax != null){
+            return tiempoTransplantarMax;
+        } else {
+            return 0;
+        }
+    }
+
+    public void setTiempoTransplantarMax(Integer tiempoTransplantar) {
+        this.tiempoTransplantarMax = tiempoTransplantar;
+    }
+    
+//    public void setTiempoParaTransplantar(){
+//        this.tiempoParaTransplantar = "";
+//        if (getTransplantarMin != 0){
+//           this.tiempoParaTransplantar = String.ValueOff(getTiempoTransplantarMin);
+//        }
+//        if (getTiempoTransplantarMax != 0) {
+//            if(getTiempoTransplantarMin == 0){    
+//               this.tiempoParaTransplantar = String.ValueOff(getTiempoTransplantarMax);
+//            } else {
+//               this.tiempoParaTransplantar = this.tiempoParaTransplantar.concat(" - ");
+//               this.tiempoParaTransplantar = this.tiempoParaTransplantar.concat(getTiempoGerminarMax);
+//            }
+//        }
+//        if (getTiempoTransplantarMin == 0 && getTiempoTransplantarMax == 0){
+//            this.tiempoParaTransplantar.concat(" - ");
+//        } else {
+//            this.tiempoParaTransplantar.concat(" días");
+//    }
+    
+//    public String getTiempoParaTransplantar () {
+//          return this.tiempoParaTransplantar;
+    
+    public Integer getTiempoCosecharMin() {
+        if (this.tiempoCosecharMin != null){
+            return tiempoCosecharMin;
+        } else {
+            return 0;
+        }
+    }
+
+    public void setTiempoCosecharMin(Integer tiempoCosechar) {
+        this.tiempoCosecharMin = tiempoCosechar;
+    }
+
+    public Integer getTiempoCosecharMax() {
+        if (this.tiempoCosecharMax != null){
+            return tiempoCosecharMax;
+        } else {
+            return 0;
+        }        
+    }
+
+    public void setTiempoCosecharMax(Integer tiempoCosechar) {
+        this.tiempoCosecharMax = tiempoCosechar;
+    }
+    
+//    public void setTiempoCosechar(){
+//        this.tiempoCosechar = "";
+//        if (getTiempoCosecharMin != 0){
+//           this.tiempoCosechar = String.ValueOff(getTiempoCosecharMin);
+//        }
+//        if (getTiempoCosecharMax != 0) {
+//            if(getTiempoCosecharMin == 0){    
+//               this.tiempoCosechar = String.ValueOff(getTiempoCosecharMax);
+//            } else {
+//               this.tiempoCosechar = this.tiempoCosechar.concat(" - ");
+//               this.tiempoCosechar = this.tiempoCosechar.concat(getTiempoCosecharMax);
+//            }
+//        }
+//        if (getTiempoCosecharMin == 0 && getTiempoCosecharMax == 0){
+//            this.tiempoCosechar.concat(" - ");
+//        } else {
+//            this.tiempoCosechar.concat(" días");
+//        }
+//    }
+    
+//    public String getTiempoCosechar () {
+//          return this.tiempoCosechar;
 }
