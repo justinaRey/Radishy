a6a14f39309700fc41f810d837b85e5012227f8f
facuh5 <facu5herraez@gmail.com>
2021-11-09 16:27:57 -0300

    servicio sesion iniciada según mi versión

--- Radishy/src/main/java/egg/Radishy/Servicios/SesionIniciada_servicio.java
+++ Radishy/src/main/java/egg/Radishy/Servicios/SesionIniciada_servicio.java
@@ -13,10 +13,8 @@ import egg.Radishy.entidades.Cultivo;
 import egg.Radishy.entidades.SesionIniciada;
 import egg.Radishy.entidades.Usuario;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,222 +25,171 @@ import org.springframework.stereotype.Service;
 @Service
 public class SesionIniciada_servicio {
     
+   // corregir método de mis cultivos
     @Autowired
-    private SesionIniciada_repositorio repositorioSesIn;
-    @Autowired
-    private Usuario_repositorio usuarioRepositorio;
+    Usuario_repositorio usuarioRepositorio;
     
     @Autowired
-    private Cultivo_repositorio cultivoRepositorio;
+    SesionIniciada_repositorio repositorioSesIn;
     
-// agregarCultivo(): agrega un cultivo a los que ya posee el usuario (recibe el id del usuario)
-    public void agregarMiCultivo (String idUsuario, String idCultivo) throws Errores_servicio{
-        
-        Optional<Usuario> rtaus = usuarioRepositorio.findById(idUsuario);
-        if (rtaus.isPresent()){
-            Usuario usuario = rtaus.get();
-            Optional<Cultivo> rtacul = cultivoRepositorio.findById(idCultivo);
-            if (rtacul.isPresent()) {
-                Cultivo cultivo = rtacul.get();
-                //'query de repositorioSesIn que busca la sesionIniciada según el usuario'
-                SesionIniciada sesion = repositorioSesIn.findByUsuario(usuario);
-                List<Cultivo> cultivos = sesion.getCultivos();
-                cultivos.add(cultivo);
-                repositorioSesIn.save(sesion);
-            } else {
-                throw new Errores_servicio("El cultivo que intenta agregar a sus cultivos, no se encuentra guardado en nuestra base de datos.\nSi quiere, puede ir a agregarla a la misma para luego incorporarlo a sus cultivos");
-           }
-        } else {
-            throw new Errores_servicio("No se encontró al usuario.");
-        }   
-    }
+    @Autowired
+    Cultivo_repositorio cultivoRepositorio;
     
-    // agregarAMisCultivos(): agrega un cultivo a los que ya posee el usuario (busca al usuario en sesion iniciada)
-    public void agregarAMisCultivos (String idCultivo, Date fechaDeSiembra) throws Errores_servicio{
-        //'query que cuenta la cantidad de usuarios con enSesion true'
-        if(usuarioRepositorio.cantidadEnSesionTrue() == 1){
-            //'query que busca el usuario con enSesion true'  ---> findByEnSesion
-            Usuario usuario = usuarioRepositorio.findByEnSesion();
-            Optional<Cultivo> rta = cultivoRepositorio.findById(idCultivo);
-            if (rta.isPresent()) {
-                if(fechaDeSiembra != null) {
-                    //'query que busca en repositorioSesIn la sesion iniciada segun el usuario' ---> findByUsuario
-                    SesionIniciada sesion = repositorioSesIn.findByUsuario(usuario);
-                    rta.get().setFechaDeSiembra(fechaDeSiembra);  // se le cambia la fecha de siembra al cultivo
-                    cultivoRepositorio.save(rta.get());  // se la guarda en el repositoiro del cultivo
-                    sesion.getCultivos().add(rta.get());  // se agrega el cultivo con la fecha modificada al usuario en sesion iniciada
-                    repositorioSesIn.save(sesion);
-                } else {
-                    throw new Errores_servicio("No ha ingresado una fecha de siembra");
-                }
-            } else {
-                throw new Errores_servicio("No se encuentra registrado el cultivo seleccionado");
-            }
-        } else {
-            throw new Errores_servicio("No puede agregar a sus cultivos debido a que no ha iniciado sesión.\nInicie su sesión o regístrese para poder hacerlo");
-        }
-        // me quedó de la duda de si no hay que sacar la lista de cultivos y hacer que sea un solo cultivo y que la fecha de siembra esté en sesión iniciada; y que por ende, se cree un sesión iniciada por cada cultivo agregado al usuario
-    }
-    
-// misCultivos(): devuelve una lista con todos los cultivos que el usuario posee (busca con el id de la sesionIniciada)
-    public List<Cultivo> misCultivos (String idSesionIniciada) {
-        SesionIniciada sesion = repositorioSesIn.findById(idSesionIniciada).get();
-        return sesion.getCultivos();
-    }
-    
-    //misCultivos(): devuelve los cultivos que posee el usuario con la sesion iniciada (sin parametros)
-    public List<Cultivo> misCultivos2 () throws Errores_servicio{
-        //'query que cuenta la cantidad de usuarios enSesion true'
-        if (usuarioRepositorio.cantidadEnSesionTrue() == 1){
-            Usuario usuario = usuarioRepositorio.findByEnSesion();
-            SesionIniciada sesion = repositorioSesIn.findByUsuario(usuario);
-            return sesion.getCultivos();
-        } else {
-            throw new Errores_servicio("No ha iniciado sesión aún");
+    // vaciarMisCultivos(): elimina todos los cultivos pertenecientes al usuario
+    public void vaciarMisCultivos () throws Errores_servicio{
+        chequearEsteSesionIniciada();
+        Usuario usuario = usuarioRepositorio.findByEnSesion();
+        List<SesionIniciada> sesionUsuario = repositorioSesIn.cultivosUsuario();
+        for (SesionIniciada sesIn : sesionUsuario) {
+            eliminarMiCultivo(sesIn.getId());
         }
     }
     
-    // eliminarMiCultivo(): recibe el id del cultivo que quiere sacar de su listado de cultivos (busca usuario por enSesion)
-    public void eliminarMiCultivo (String idCultivo) throws Errores_servicio{
-        //'query que cuenta la cantidad de usuarios con enSesion true'
-        if(usuarioRepositorio.cantidadEnSesionTrue() == 1){
-            //'query que busca al usuario segun enSesion true' ---> findByEnSesion
-            Usuario usuario = usuarioRepositorio.findByEnSesion();
-            //'query que busca la sesionIniciada en su repositorio s/el usuario' ---> findByUsuario
-            SesionIniciada sesion = repositorioSesIn.findByUsuario(usuario);
-            Iterator<Cultivo> itcultivos = sesion.getCultivos().iterator();
-            while(itcultivos.hasNext()){
-                if (itcultivos.next().getId().equals(idCultivo)){
-                    itcultivos.remove();
-                    break;
-                }
-            }
-        } else {
-            throw new Errores_servicio("No hay ningún usuario ingresado, por ende no podrá eliminar ningún cultivo");
-        }
-    }
-    
-    //eliminarUnCultivo(): recibe el id del cultivo a eliminar del usuario recibido
-    public void eliminarUnCultivo (String idCultivo, String idUsuario) throws Errores_servicio{
-        Optional<Usuario> rta = usuarioRepositorio.findById(idUsuario);
+    // eliminarMiCultivo(): elimina al cultivo de la lista de cultivos del usuario seleccionado
+    public void eliminarMiCultivo (String idSesIn) throws Errores_servicio{
+        chequearEsteSesionIniciada();
+        Optional<SesionIniciada> rta = repositorioSesIn.findById(idSesIn);
         if (rta.isPresent()){
-            Usuario usuario = rta.get();
-            //'query que busca en repositorioSesIn la sesion iniciada segun el usuario' ---> findByUsuario
-            SesionIniciada sesion = repositorioSesIn.findByUsuario(usuario);
-            Iterator<Cultivo> cultivos = sesion.getCultivos().iterator();
-            while (cultivos.hasNext()){
-                if (cultivos.next().getId().equals(idCultivo)){
-                    cultivos.remove();
-                    break;
+            repositorioSesIn.delete(rta.get());
+        } else {
+            throw new Errores_servicio("No se encontró el cultivo que desea eliminar de sus cultivos");
+        }
+    }
+    
+    // agregarMiCultivo(): recibe el id de
+    public void agregarMiCultivo (String idCultivo, Date fechaSembrado) throws Errores_servicio{
+        chequearEsteSesionIniciada();
+        if (idCultivo != null && fechaSembrado != null){
+            SesionIniciada sesion = new SesionIniciada();
+            Cultivo cultivo = cultivoRepositorio.findById(idCultivo).get();
+            Usuario usuario = usuarioRepositorio.findByEnSesion();
+            sesion.setCultivo(cultivo);
+            sesion.setUsuario(usuario);
+            sesion.setFechaDeSembrado(fechaSembrado);
+            repositorioSesIn.save(sesion);
+        } else {
+            throw new Errores_servicio("Todos los campos deben estar completos, revise nuevamente el formulario");
+        }
+    }
+    
+    // misCultivos(): devuelve una lista con todos los cultivos que posee el usuario en sesión
+    public List<SesionIniciada> misCultivos() throws Errores_servicio{
+        chequearEsteSesionIniciada();
+        List<SesionIniciada> cultivosSesIn = repositorioSesIn.cultivosUsuario();
+        if (cultivosSesIn.isEmpty()){
+            throw new Errores_servicio("No posee cultivos aún");
+        } else {
+            return cultivosSesIn;
+        }
+    }
+    
+    /*         Todos los campos por separado que hacen ha los datos a mostrar por pantalla de 'mis cultivos'        */
+    
+    public String nombreCultivo (SesionIniciada sesion){
+        return sesion.getCultivo().getNombre();
+    }
+    
+    public String metodoCultivo(SesionIniciada sesion){
+        return sesion.getCultivo().getMetodo();
+    }
+    
+    public int profundidadCultivo (SesionIniciada sesion) {
+        return sesion.getCultivo().getProfundidadSiembraCM();
+    }
+    
+//    public String riegoCultivo (SesionIniciada sesion) {
+//        return sesion.getCultivo().getRiego();
+//    }
+    
+    public Date fechaSembradoCultivo (SesionIniciada sesion){
+        return sesion.getFechaDeSembrado();
+    }
+    
+//    public Date fechaGerminadoCultivo (SesionIniciada sesion) {
+//        return sesion.getFechaGermiadoProm();
+//    }
+//    
+//    public Date fechaTransplanteCultivo (SesionIniciada sesion) {
+//        return sesion.getFechaTransplanteProm();
+//    }
+//    
+//    public Date fechaCosechaCultivo (SesionIniciada sesion) {
+//        return sesion.getFechaCosechaAprox();
+//    }
+    
+    /*              métodos para iniciar la sesión y cerrarla                 */
+    
+    // iniciarSesion(): inicia la sesión del usuario según los datos ingresados
+    public void iniciarSesion(String usuario, String password) throws Errores_servicio_ {
+        chequeoEstenSesionesCerradas();
+        if (usuario != null) {
+            if (usuarioRepositorio.cantidadUsuariosNombre(usuario) != 0) {
+                Usuario usu = usuarioRepositorio.findByNombreUsuario(usuario);
+                if (password != null){
+                    String contrasenia = usu.getPassword();
+                    if (contrasenia.equals(password)){
+                        usu.setEnSesion(true);
+                        usuarioRepositorio.save(usu);
+                    } else {
+                        throw new Errores_servicio("El usuario y/o contraseña introducidos no son correctos");
+                    }
+                } else {
+                    throw new Errores_servicio("No ha introducido ninguna contraseña");
                 }
-            }
-            repositorioSesIn.save(sesion);
-        } else {
-            throw new Errores_servicio("No se encontró al usuario solicitado");
-        }
-    }
-    
-    // vaciarMisCultivos(): recibe el id del usuario en cuestión y vacía su lista de cultivos seleccionados
-    public void vaciarMisCultivos (String idUsuario) throws Errores_servicio{
-        Optional<Usuario> rta = usuarioRepositorio.findById(idUsuario);
-        if(rta.isPresent()){
-            Usuario usuario = rta.get();
-            //'query que busca la sesionIniciada según el usuario' ---> findByUsuario
-            SesionIniciada sesion = repositorioSesIn.findByUsuario(usuario);
-            sesion.getCultivos().clear();
-            repositorioSesIn.save(sesion);
-        } else {
-            throw new Errores_servicio("No se encontró al usuario solicitado");
-        }
-    }
-    
-    // vaciarMisCultivos2(): al usuario con la sesión iniciada se le quitan todos sus cultivos seleccionados
-    public void vaciarMisCultivos2 () throws Errores_servicio{
-        //'query que cuenta la cantidad de usuarios enSesion true'
-        if(usuarioRepositorio.cantidadEnSesionTrue() == 1){
-            //  'query que busca al usuario según enSesion' ---> findByEnSesion
-            Usuario usuario = usuarioRepositorio.findByEnSesion();
-            //  'query que busca en repositorioSesIn la sesion iniciada segun el usuario' ---> findByUsuario
-            SesionIniciada sesion = repositorioSesIn.findByUsuario(usuario);
-            sesion.getCultivos().clear();
-            repositorioSesIn.save(sesion);
-        } else {
-            throw new Errores_servicio("No ha iniciado sesión todavía");
-        }
-    }
-    
-    // incorporarle un boolean al usuario de 'en sesion' para que sea false siempre, salvo cuando ese usuario está en sesión
-    
-// iniciarSesion(): recibe el nombre de usuario y la contraseña por parte del 'usuario' y controla que se pueda iniciar la sesión y los datos ingresados sean correctos
-    @Transactional
-    public void iniciarSesion (String usuario, String password) throws Errores_servicio{
-        //'query que cuente la cantidad de usarios con enSesion true'
-        if (usuarioRepositorio.cantidadEnSesionTrue() < 1) {
-            validarHayaDatosInicioSesion(usuario, password);
-            //  'query que busque al usuario en el repositorio con el nombre de usuario' ---> findByNombreUsuario(nombreUsuario)
-            Usuario datosUsuario = usuarioRepositorio.findByNombreUsuario(usuario);
-            if (datosUsuario.getPassword() == password) {
-                datosUsuario.setEnSesion(true);
-                usuarioRepositorio.save(datosUsuario);
             } else {
-                throw new Errores_servicio("La contraseña ingresada no corresponde al nombre de usuario ingresado");
+                throw new Errores_servicio("No se ha encontrado ningún usuario con ese 'nombre de usuario'");
             }
         } else {
-            throw new Errores_servicio("No se puede tener dos sesiones ingresadas a la vez, por favor cierre la sesión en curso antes de iniciar una nueva");
+            throw new Errores_servicio("No ha introducido un nombre de usuario");
         }
     }
     
-    // cerrarSesion(): recibe el id del Usuario activo y le cierra la sesión
-    public void cerrarSesion (String idUsuario) {
-        Usuario usuario = usuarioRepositorio.findById(idUsuario).get();
+    // cerrarSesion(): cierra la sesion del usuario que esté abierta
+    public void cerrarSesion () throws Errores_servicio{
+        chequearEsteSesionIniciada();
+    //    if (usuarioRepositorio.cantidadEnSesionTrue() != 0) {
+        Usuario usuario = usuarioRepositorio.findByEnSesion();
         usuario.setEnSesion(false);
+        usuarioRepositorio.save(usuario);
+    //    } else {
+    //        throw new Error_servicio_("No se puede cerrar sesión si no hay ninguna sesión iniciada");
+    //    }   
     }
     
-    // cerrarSesion2(): a la sesion iniciada, la cierra, sin requerimiento de ningún parámetro
-    public void cerrarSesion2 () throws Errores_servicio{
-        //'query que busca la cant de usuarios con enSesion true'
-        if(usuarioRepositorio.cantidadEnSesionTrue() == 1){
-            //  'query que busca al usuario segun enSesion true' ---> findByEnSesion
-            Usuario usuario = usuarioRepositorio.findByEnSesion();
-            usuario.setEnSesion(false);
-            usuarioRepositorio.save(usuario);
-        } else {
-            throw new Errores_servicio("No había ninguna sesión iniciada que cerrar");
+    
+    
+    /*               control de que estén todas las sesiones cerradas         */
+    
+    // chequeoEstenSesionesCerradas(): corrobora que no haya ninguna sesión iniciada
+    public void chequeoEstenSesionesCerradas() throws Errores_servicio{
+        if (usuarioRepositorio.cantidadEnSesionTrue() != 0){
+            throw new Errores_servicio("Ya hay una sesión iniciada.\nCiérrela para poder iniciar sesión con otra cuenta de usuario.");
         }
     }
     
-    // validarHayaDatosInicioSesion(): 
-    public void validarHayaDatosInicioSesion(String usuario, String password) throws Errores_servicio {
-        if (usuario == null) {
-            throw new Errores_servicio("No ha ingresado un nombre de usuario");
-        }
-        if (password == null) {
-            throw new Errores_servicio("No ha ingresado una contraseña");
+    
+    
+    /*               control de que haya una sesión iniciada                  */
+    
+    // chequearEsteSesionIniciada(): corrobora que haya una sesión iniciada
+    public void chequearEsteSesionIniciada () throws Errores_servicio{
+        int cantSesIn = usuarioRepositorio.cantidadEnSesionTrue();
+        if (cantSesIn != 1){
+            if (cantSesIn < 1) {
+                throw new Errores_servicio("No ha iniciado sesión");
+            } else {
+                List<Usuario> sesionesIniciadas = usuarioRepositorio.findSesionesIniciadas();
+                for (Usuario sesionIniciada : sesionesIniciadas) {
+                    cerrarSesiones(sesionIniciada);
+                }
+                throw new Errores_servicio("No ha iniciado sesión");
+            }
         }
     }
     
-    // editarCuenta(): recibe el id de un usuario (se puede modificar haciendo que se busque al usuario 'enSesion' y sea al que se modifica) y los campos que se le permiten editar al usuario, para modificar los datos de aquellos que ingrese algo
-    public void editarCuenta (String idUsuario, String usuario, String passwordActual, String passwordNueva, String apodo) throws Errores_servicio{
-        Optional<Usuario> rta = usuarioRepositorio.findById(idUsuario);
-        if (rta.isPresent()) {
-            Usuario datosUsuario = rta.get();
-            if (passwordActual == null) {
-                throw new Errores_servicio("No puede guardar ningún cambio si no introduce su contraseña actual");
-            }
-            if (usuario != null) {
-             datosUsuario.setNombre(usuario);
-            }
-            if (passwordNueva != null) {
-                datosUsuario.setPassword(passwordNueva);
-            }
-            if(apodo != null) {
-             datosUsuario.setApodo(apodo);
-            }
-            usuarioRepositorio.save(datosUsuario);
-        } 
+    // cerrarSesiones(): se utiliza en el chequeo de las sesiones iniciadas, recibe un usuario que debe cerrarle la sesion
+    public void cerrarSesiones (Usuario u) {
+        u.setEnSesion(false);
+        usuarioRepositorio.save(u);
     }
-    
-    // query en repositorio del usuario que cuente la cantidad de usuarios con 'enSesion' = true, y devuelva dicha cantidad 
-    // query en repositorio del usuario que busque y devuelva al usuario con 'enSesion' = true  ---> findByEnSesion()
-    // query en repositorio del usuario que busca al usuario según el nombre del usuario  ---> findByNombreUsuario()
 }
