b9cf799cbe0c800620eda6f22591e4864ab7e819
MauroGobernatori <mauro.gobernatori@gmail.com>
2021-11-23 14:06:42 -0300

    Progreso

--- Radishy/src/main/java/egg/Radishy/Controladores/Main_controller.java
+++ Radishy/src/main/java/egg/Radishy/Controladores/Main_controller.java
@@ -4,6 +4,11 @@ package egg.Radishy.Controladores;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import egg.Radishy.Servicios.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.mail.javamail.JavaMailSender;
 
 /**
  *
@@ -12,6 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class Main_controller {
+    
+    @Autowired // uS ---> servicio del usuario
+    private MailService mS;
+
     @GetMapping("")
     public String index(){
         return "index";
@@ -26,4 +35,11 @@ public class Main_controller {
     public String infoNuestra(){
         return "info";
     }
+
+    @PostMapping("enviar-mail")
+    public String enviarEmail(@RequestParam String destinatario, @RequestParam String asunto, @RequestParam String contenido) {
+        mS.enviarMail(destinatario, asunto, contenido);
+        return "/send-email";
+    }
+
 }
